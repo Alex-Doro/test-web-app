a6c103b45518005e25d7ec482fc3d62333fe55d9
Alex-Doro <alexandr.doroshenko13@gmail.com>
2023-05-12 22:15:56 +0300

    add font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import { AppProps } from "next/app";
+import "normalize.css/normalize.css";
+import "../styles/main.scss";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
