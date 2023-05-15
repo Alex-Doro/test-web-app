cebe0bbec621c6b3621f9b5f46705ad7b336b6c7
Alex-Doro <alexandr.doroshenko13@gmail.com>
2023-05-15 17:11:06 +0300

    add Input element

--- components/partials/NewProject/StartProject/StartProject.tsx
+++ components/partials/NewProject/StartProject/StartProject.tsx
@@ -1,5 +1,7 @@
-import styles from "./StartProject.module.scss";
 import cn from "classnames";
+
+import Input from "@/components/shared/Input/Input";
+import styles from "./StartProject.module.scss";
 
 const StartProject: React.FC = () => {
   return (
@@ -8,6 +10,22 @@ const StartProject: React.FC = () => {
         To Create Quest you need firstly create Project
       </p>
       <h2 className={styles.title}>Add New Project</h2>;
+      <Input
+        type="text"
+        name="projectName"
+        id="projectName"
+        label="Project Name (It can be changed later)"
+        placeholder="Enter a project name"
+        value=""
+      />
+      <Input
+        type="text"
+        name="projectURL"
+        id="projectURL"
+        label="Project URL (It cannot be changed after creation)"
+        placeholder="Alphaguilty.io/awesomenftpunch"
+        value=""
+      />
     </div>
   );
 };
---
+++ components/shared/Input/Input.tsx
@@ -0,0 +1,35 @@
+import cn from "classnames";
+
+import styles from "./Input.module.scss";
+
+const Input: React.FC<{
+  name: string;
+  id: string;
+  type: string;
+  placeholder: string;
+  value: string | number;
+  label?: string;
+  onChange?: () => void;
+}> = ({ name, id, type, placeholder, label, value, onChange }) => {
+  return (
+    <div className={styles.root}>
+      {label && (
+        <label htmlFor={id} className={cn("text", styles.label)}>
+          {label}
+        </label>
+      )}
+      <input
+        className={cn("text", styles.input)}
+        type={type}
+        name={name}
+        id={id}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+      ;
+    </div>
+  );
+};
+
+export default Input;
