c036f367e765ec7e3c73747006afcae5672f7e26
Alex-Doro <alexandr.doroshenko13@gmail.com>
2023-05-15 16:02:32 +0300

    fix new project layout styles

--- components/partials/NewProject/NewProject.tsx
+++ components/partials/NewProject/NewProject.tsx
@@ -3,16 +3,18 @@ import { useSelector } from "react-redux";
 import { RootState } from "@/store";
 import Steps from "../Steps/Steps";
 
+import styles from "./NewProject.module.scss";
+
 const NewProject = () => {
   const newProjectState = useSelector((state: RootState) => state.newProject);
 
   console.log(newProjectState);
 
   return (
-    <>
+    <section className={styles.root}>
       <Steps steps={newProjectState.steps}></Steps>
       <h1>New Project</h1>
-    </>
+    </section>
   );
 };
 
