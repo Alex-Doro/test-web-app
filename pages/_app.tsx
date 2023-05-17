804f5efdad6c1f7a57f59db3a9612584b84c1f9c
Alex-Doro <alexandr.doroshenko13@gmail.com>
2023-05-17 17:36:01 +0300

    remove store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,8 @@
 import { AppProps } from "next/app";
 import "../styles/index.scss";
-import { wrapper } from "@/store";
-import { Provider } from "react-redux";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const { store } = wrapper.useWrappedStore(pageProps);
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
