3143626cb0a40907dfa55d7c4e462654d0d733fe
Alex-Doro <alexandr.doroshenko13@gmail.com>
2023-05-17 15:10:05 +0300

    add Counter logic

--- components/shared/Counter/Counter.tsx
+++ components/shared/Counter/Counter.tsx
@@ -1,7 +1,9 @@
 import cn from "classnames";
+import { useRef } from "react";
+import { useFormContext } from "react-hook-form";
 
 import styles from "./Counter.module.scss";
-import { useFormContext } from "react-hook-form";
+
 import Button from "../Button/Button";
 import Input from "../Input/Input";
 
@@ -10,13 +12,31 @@ const Counter: React.FC<{
 }> = ({ name }) => {
   const form = useFormContext();
 
+  const handleCounter = (action: "increment" | "decrement") => {
+    if (action === "increment") {
+      form.setValue(name, form.getValues(name) + 1);
+    }
+    if (action === "decrement") {
+      if (form.getValues(name) === 0) return;
+      form.setValue(name, form.getValues(name) - 1);
+    }
+  };
+
   return (
     <div className={styles.root}>
-      <Button variant="secondary" className={styles.counterBtn}>
+      <Button
+        variant="secondary"
+        className={styles.counterBtn}
+        onClick={() => handleCounter("decrement")}
+      >
         -
       </Button>
       <Input name={name} id={name} type="number" className={styles.counter} />
-      <Button variant="secondary" className={styles.counterBtn}>
+      <Button
+        variant="secondary"
+        className={styles.counterBtn}
+        onClick={() => handleCounter("increment")}
+      >
         +
       </Button>
     </div>
